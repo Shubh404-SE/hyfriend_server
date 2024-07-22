1d19dc40c123e29ec2147587670cd0fcb8218b77
Shubh404-SE <tanwarshubham29@gmail.com>
2024-07-23 00:29:12 +0530

    generate token for zego cloud service..

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,4 +1,5 @@
 import getPrismaInstance from "../utils/PrismaClient.js";
+import {generateToken04} from "../utils/TokenGenerator.js";
 
 export const checkUser = async (req, res, next) => {
   try {
@@ -62,3 +63,22 @@ export const getAllUsers = async (req, res, next) => {
     next(err);
   }
 };
+
+
+export const generateToken = (req, res, next)=>{
+  try{
+    const appId = parseInt(process.env.ZEGO_APP_ID);
+    const searverSecret = process.env.ZEGO_SERVER_ID;
+    const userId = req.params.userId;
+    const effectiveTime = 3600;
+    const payload = "";
+    // console.log(appId, searverSecret, userId);
+    if(appId && searverSecret && userId){
+       const token = generateToken04(appId, userId, searverSecret, effectiveTime, payload);
+      res.status(200).json({token});
+    }
+    res.status(400).send("User id, app id and searver id is required");
+  }catch(err){
+    next(err);
+  }
+} 
