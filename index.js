32f76adc1586061ce950edf995c84f8886b1b269
Shubh404-SE <tanwarshubham29@gmail.com>
2024-07-27 16:04:41 +0530

    ....

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
 });
 
 app.use(cors({
-  origin: process.env.CLIENT_URL, // Ensure this is the correct URL
+  origin: [process.env.CLIENT_URL, "http://localhost:3000"], // Ensure this is the correct URL
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "application/json", "text/plain", "*/*", "*"],
 }));
