fb397fcb28a9e3afd51a922878061b318b8cf19c
Shubh404-SE <tanwarshubham29@gmail.com>
2024-07-27 15:49:27 +0530

    corsssss.... :)

--- index.js
+++ index.js
@@ -12,8 +12,9 @@ const app = express();
 app.use(cors({
   origin: process.env.CLIENT_URL, // Ensure this is the correct URL
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization", "application/json", "text/plain", "*/*"],
+  allowedHeaders: ["Content-Type", "Authorization", "application/json", "text/plain", "*/*", "*"],
 }));
+app.options('*', cors()); // Handle preflight requests
 app.use(express.json());
 
 app.use("/uploads/recordings", express.static("uploads/recordings"));
