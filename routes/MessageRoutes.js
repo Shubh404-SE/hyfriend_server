7a214c1778ce449d35cc20e6d1be526433ab7124
Shubh404-SE <tanwarshubham29@gmail.com>
2024-08-16 17:04:00 +0530

    function not defined error solved

--- routes/MessageRoutes.js
+++ routes/MessageRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { addAudioMessage, addImageMessage, addMessage, addMessageReaction, getInitialContactsWithMessages, getMessages } from "../controllers/MessageController.js";
+import { addAudioMessage, addImageMessage, addMessage, getInitialContactsWithMessages, getMessages } from "../controllers/MessageController.js";
 import multer from "multer";
 
 const router = Router();
@@ -9,7 +9,7 @@ const uploadAudio = multer({dest:"uploads/recordings"});
 
 router.post("/add-message", addMessage);
 router.get("/get-messages/:from/:to", getMessages);
-router.post("/add-message-reaction", addMessageReaction);
+// router.post("/add-message-reaction", addMessageReaction);
 router.get("/get-initial-contacts/:from", getInitialContactsWithMessages);
 router.post("/add-image-message", uploadImage.single("image"), addImageMessage);
 router.post("/add-audio-message", uploadAudio.single("audio"), addAudioMessage);
