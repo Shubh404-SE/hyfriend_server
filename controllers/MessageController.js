1f7a2a42378ec50ef0948e597a2a3030bad53b98
Shubh404-SE <tanwarshubham29@gmail.com>
2024-08-16 17:09:20 +0530

    react-message api working

--- controllers/MessageController.js
+++ controllers/MessageController.js
@@ -1,6 +1,6 @@
 // messageController.js
-import { query } from '../postgres/db.js';
-import fs from 'fs';
+import { query } from "../postgres/db.js";
+import fs from "fs";
 
 export const addMessage = async (req, res, next) => {
   try {
@@ -13,12 +13,44 @@ export const addMessage = async (req, res, next) => {
         VALUES ($1, $2, $3, $4)
         RETURNING *;
       `;
-      const values = [message, parseInt(from), parseInt(to), getUser ? "delivered" : "sent"]; // set messageStatus if user online or not. can create message delivered time. then we will have three times sent time (actual time), delivered time(when it is delivered) and seen time (when message is actually seen)
+      const values = [
+        message,
+        parseInt(from),
+        parseInt(to),
+        getUser ? "delivered" : "sent",
+      ]; // set messageStatus if user online or not. can create message delivered time. then we will have three times sent time (actual time), delivered time(when it is delivered) and seen time (when message is actually seen)
       const { rows } = await query(queryText, values);
 
       return res.status(201).send({ message: rows[0] });
     }
     return res.status(400).send("From, to and message are required");
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Controller to add a reaction to a message
+export const addMessageReaction = async (req, res, next) => {
+  try {
+    const { messageId, userId, reaction } = req.body;
+
+    if (!messageId || !userId || !reaction) {
+      return res
+        .status(400)
+        .json({ error: "messageId, userId, and reaction are required" });
+    }
+
+    const insertReactionQuery = `
+      INSERT INTO "MessageReactions" ("messageId", "userId", "reaction")
+      VALUES ($1, $2, $3)
+      ON CONFLICT ("messageId", "userId") 
+      DO UPDATE SET "reaction" = EXCLUDED.reaction
+      RETURNING *;
+    `;
+    const values = [messageId, userId, reaction];
+    const { rows } = await query(insertReactionQuery, values);
+
+    return res.status(201).json({ reaction: rows[0] });
   } catch (err) {
     next(err);
   }
@@ -36,16 +68,21 @@ export const getMessages = async (req, res, next) => {
     const values = [parseInt(from), parseInt(to)];
     const { rows: messages } = await query(queryText, values);
 
-    const unreadMessages = []; 
+    const unreadMessages = [];
 
-    messages.forEach((message, index) => { // get unreadmessages and mark them as read
-      if (message.messageStatus !== "read" && message.senderId === parseInt(to)) {
+    messages.forEach((message, index) => {
+      // get unreadmessages and mark them as read
+      if (
+        message.messageStatus !== "read" &&
+        message.senderId === parseInt(to)
+      ) {
         messages[index].messageStatus = "read";
         unreadMessages.push(message.id);
       }
     });
 
-    if (unreadMessages.length > 0) { // set messages seen time here - default it will be null or nothing
+    if (unreadMessages.length > 0) {
+      // set messages seen time here - default it will be null or nothing
       const updateText = `
         UPDATE "Messages"
         SET "messageStatus" = 'read'
@@ -64,7 +101,8 @@ export const addImageMessage = async (req, res, next) => {
   try {
     if (req.file) {
       const date = Date.now();
-      let fileName = "uploads/images/" + "HyFriend-image-"+ date + req.file.originalname; // unique name
+      let fileName =
+        "uploads/images/" + "HyFriend-image-" + date + req.file.originalname; // unique name
       fs.renameSync(req.file.path, fileName);
 
       const { from, to } = req.query;
@@ -92,7 +130,11 @@ export const addAudioMessage = async (req, res, next) => {
   try {
     if (req.file) {
       const date = Date.now();
-      let fileName = "uploads/recordings/" + "HyFriend-audio-" + date + req.file.originalname;
+      let fileName =
+        "uploads/recordings/" +
+        "HyFriend-audio-" +
+        date +
+        req.file.originalname;
       fs.renameSync(req.file.path, fileName);
 
       const { from, to } = req.query;
@@ -116,29 +158,40 @@ export const addAudioMessage = async (req, res, next) => {
   }
 };
 
-
 export const getInitialContactsWithMessages = async (req, res, next) => {
   try {
     const userId = parseInt(req.params.from);
 
     // Fetch the user
-    const userResult = await query(`
+    const userResult = await query(
+      `
       SELECT * FROM "User" WHERE id = $1
-    `, [userId]);
+    `,
+      [userId]
+    );
 
     const user = userResult.rows[0];
 
     // Fetch sent and received messages
-    const sentMessagesResult = await query(`
+    const sentMessagesResult = await query(
+      `
       SELECT * FROM "Messages" WHERE "senderId" = $1 ORDER BY "createdAt" DESC
-    `, [userId]);
+    `,
+      [userId]
+    );
 
-    const receivedMessagesResult = await query(`
+    const receivedMessagesResult = await query(
+      `
       SELECT * FROM "Messages" WHERE "recieverId" = $1 ORDER BY "createdAt" DESC
-    `, [userId]);
+    `,
+      [userId]
+    );
 
-    const messages = [...sentMessagesResult.rows, ...receivedMessagesResult.rows];
-    messages.sort((a, b) => b.createdAt - a.createdAt); 
+    const messages = [
+      ...sentMessagesResult.rows,
+      ...receivedMessagesResult.rows,
+    ];
+    messages.sort((a, b) => b.createdAt - a.createdAt);
 
     const users = new Map();
     const messageStatusChange = [];
@@ -147,14 +200,18 @@ export const getInitialContactsWithMessages = async (req, res, next) => {
       const isSender = msg.senderId === userId;
       const calculatedId = isSender ? msg.recieverId : msg.senderId;
 
-      if (msg.messageStatus === 'sent') {
+      if (msg.messageStatus === "sent") {
         messageStatusChange.push(msg.id);
       }
 
-      if (!users.has(calculatedId)) { // if user is not in users then unreadmessages
-        const userResult = await query(`
+      if (!users.has(calculatedId)) {
+        // if user is not in users then unreadmessages
+        const userResult = await query(
+          `
           SELECT id, email, name, "profilePicture", about FROM "User" WHERE id = $1
-        `, [calculatedId]);
+        `,
+          [calculatedId]
+        );
 
         const userObj = userResult.rows[0];
 
@@ -166,12 +223,17 @@ export const getInitialContactsWithMessages = async (req, res, next) => {
           createdAt: msg.createdAt,
           senderId: msg.senderId,
           recieverId: msg.recieverId,
-          totalUnreadMessages: isSender ? 0 : (msg.messageStatus !== 'read' ? 1 : 0),
+          totalUnreadMessages: isSender
+            ? 0
+            : msg.messageStatus !== "read"
+            ? 1
+            : 0,
           ...userObj,
         };
 
         users.set(calculatedId, user);
-      } else if (msg.messageStatus !== 'read' && !isSender) { // is user is in  users and not sender then increase unreadmessage count
+      } else if (msg.messageStatus !== "read" && !isSender) {
+        // is user is in  users and not sender then increase unreadmessage count
         const user = users.get(calculatedId);
         users.set(calculatedId, {
           ...user,
@@ -180,14 +242,18 @@ export const getInitialContactsWithMessages = async (req, res, next) => {
       }
     }
 
-    if (messageStatusChange.length) { // change all received message status to delivered when user comes to online  
-      await query(`
+    if (messageStatusChange.length) {
+      // change all received message status to delivered when user comes to online
+      await query(
+        `
         UPDATE "Messages" SET "messageStatus" = 'delivered'
         WHERE id = ANY($1::int[])
-      `, [messageStatusChange]);
+      `,
+        [messageStatusChange]
+      );
     }
 
-    console.log('Users Map Size:', users.size); // Debug log
+    console.log("Users Map Size:", users.size); // Debug log
     res.status(200).json({
       users: Array.from(users.values()),
       onlineUsers: Array.from(onlineUsers.keys()),
--- postgres/createTable.js
+++ postgres/createTable.js
@@ -19,20 +19,27 @@ export const createTables = async () => {
     await client.query(`
       CREATE TABLE IF NOT EXISTS "Messages" (
       id SERIAL PRIMARY KEY,
-      "senderId" INT NOT NULL,
-      "recieverId" INT NOT NULL,
+      "senderId" INTEGER REFERENCES "User"(id) ON DELETE CASCADE,
+      "recieverId" INTEGER REFERENCES "User"(id) ON DELETE CASCADE,
       type VARCHAR(50) DEFAULT 'text',
       message TEXT NOT NULL,
       "messageStatus" VARCHAR(50) DEFAULT 'sent',
       "createdAt" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-      CONSTRAINT fk_sender
-        FOREIGN KEY("senderId") 
-          REFERENCES "User"(id),
-      CONSTRAINT fk_reciever
-        FOREIGN KEY("recieverId") 
-          REFERENCES "User"(id)
+      "replyToMessageId" INTEGER REFERENCES "Messages"(id) ON DELETE SET NULL DEFAULT NULL,
+      "replyToUserId" INTEGER REFERENCES "User"(id) ON DELETE SET NULL DEFAULT NULL
     );
     `);
+
+    await client.query(`
+    CREATE TABLE IF NOT EXISTS "MessageReactions" (
+    id SERIAL PRIMARY KEY,
+    "messageId" INTEGER REFERENCES "Messages"(id) ON DELETE CASCADE,
+    "userId" INTEGER REFERENCES "User"(id) ON DELETE CASCADE,
+    reaction VARCHAR(50),
+    "createdAt" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE ("messageId", "userId")
+);
+      `);
 
     console.log("Tables created successfully (if they didn't already exist)!");
   } catch (err) {
